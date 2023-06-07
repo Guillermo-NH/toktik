2f624e3ce2bb2f9d8b9f5802211a5619396ce652
Guillermo-NH <ingnanco@gmail.com>
2023-06-07 02:22:21 -0600

    gradiente de fondo

--- lib/presentation/widgets/shared/video_scrollable_view.dart
+++ lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import 'package:toktik/domain/entities/video_post.dart';
+import 'package:toktik/presentation/widgets/shared/video_button.dart';
+import 'package:toktik/presentation/widgets/video/fullscreen_player.dart';
 
 class VideoScrollableView extends StatelessWidget {
   final List<VideoPost> videos;
@@ -14,7 +16,28 @@ class VideoScrollableView extends StatelessWidget {
   Widget build(BuildContext context) {
     return PageView.builder(
       scrollDirection: Axis.vertical,
-      itemBuilder: (context, index) {},
+      physics:
+          const BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
+      itemCount: videos.length,
+      itemBuilder: (context, index) {
+        final VideoPost videoPost = videos[index];
+        return Stack(
+          children: [
+            // video player
+
+            SizedBox.expand(
+              child: FullScreenPlayer(
+                  caption: videoPost.caption, videoUrl: videoPost.videoUrl),
+            ),
+
+            //Buttons
+            Positioned(
+                bottom: 50,
+                right: 10,
+                child: VideoButtons(videoPost: videoPost))
+          ],
+        );
+      },
     );
   }
 }
---
+++ lib/presentation/widgets/video/video_background.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class VideoBackground extends StatelessWidget {
+  final List<Color> colors;
+  final List<double> stops;
+  const VideoBackground(
+      {super.key,
+      this.colors = const [Colors.transparent, Colors.black],
+      this.stops = const [0.0, 1.0]});
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned.fill(
+        child: DecoratedBox(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    stops: stops,
+                    colors: colors))));
+  }
+}
